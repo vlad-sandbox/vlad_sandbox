decc652993fb5eaa20b4c9c7bb3dec8beff59e46
Alexey <Zimin>
2019-04-15 23:48:58 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
       <p>Лешкины сосцы</p>
+    <p>сосцы-молодцы</p>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
