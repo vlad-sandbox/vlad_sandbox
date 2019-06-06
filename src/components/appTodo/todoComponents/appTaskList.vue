127e7682853997be02201a1b16cce08f2c90f675
Alexey <Zimin>
2019-06-06 19:55:00 +0300

    FEAT fish app task list

---
+++ src/components/appTodo/todoComponents/appTaskList.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  appList  default container
+</div>
+</template>
+<script>
+export default {
+  name: 'app-task-list',
+  props: ['data'],
+  data () {
+    return {
+    }
+  },
+  watch: {
+  },
+  methods: {
+  }
+}
+</script>
+<style scoped lang="stylus">
+</style>
