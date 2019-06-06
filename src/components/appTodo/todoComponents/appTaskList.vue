35c3fce3feb161c25ebc0cb5c40fc0029d3a8dd5
Vladislav <Fauler1904@mail.ru>
2019-06-06 20:31:41 +0300

    Получение и отправка

--- src/components/appTodo/todoComponents/appTaskList.vue
+++ src/components/appTodo/todoComponents/appTaskList.vue
@@ -1,12 +1,13 @@
 <template>
-<div>
-  appList  default container
+  <div v-for="(item) in data">
+    <div v-for="(tasks) in task">    
 </div>
 </template>
 <script>
+import appTaskList from './todoComponents/appTask'
 export default {
   name: 'app-task-list',
-  props: ['data'],
+  props: ['task'],
   data () {
     return {
     }
