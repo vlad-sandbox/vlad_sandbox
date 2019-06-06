384645117b643760976c2bf65db65be2c561213b
Vladislav <Fauler1904@mail.ru>
2019-06-07 00:43:14 +0300

    Стили блока задачи

--- src/components/appTodo/todoComponents/appTask.vue
+++ src/components/appTodo/todoComponents/appTask.vue
@@ -4,7 +4,7 @@
     <div class="wrappermintask">
       <div class="taskname">{{task.name}}</div>
       <div class="taskplanning">{{task.date_planning}}</div>
-      <div class="taskstatus">{{task.status}}</div>
+      <div class="taskstatusoff" :class=task.status><!--{{task.status}}--></div>
     </div>
     <!-- <div>{{task.date_start}}</div>
     <div>{{task.description}}</div> -->
@@ -17,22 +17,43 @@ export default {
   props: ['task'],
   data () {
     return {
+      prufe: false
     }
   },
   watch: {
   },
   methods: {
+    taskstatusaprufe () {
+      if (this.task.status === open) {
+        this.prufe = true
+      }
+      console.log(this.prufe)
+    }
   },
   components: {
   }
 }
 </script>
 <style scoped lang="stylus">
+.taskname, .taskplanning
+  flex-grow 1
 .wrappermintask {
   display flex
+  justify-content space-between
+  align-items center
   border 1px solid
-  border-radius 20px
-  margin-bottom 10px
+  border-radius 10px
+  margin 10px
+  padding 20px
   word-wrap break-word
 }
+.close, .open
+  width 20px
+  height 20px
+  border-radius 20px
+  border 1px solid green
+  margin-top 5px
+  margin-right 5px
+.open
+  border 1px solid red
 </style>
