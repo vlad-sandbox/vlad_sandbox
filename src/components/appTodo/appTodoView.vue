9d83d655d8ec1e15766c39ba481987cb6843881c
Alexey <Zimin>
2019-06-06 20:06:02 +0300

    FEAT подключение appTodoList

--- src/components/appTodo/appTodoView.vue
+++ src/components/appTodo/appTodoView.vue
@@ -1,21 +1,22 @@
 <template>
-  <div>
-    hi
-  </div>
+    <app-task-list :data="data"></app-task-list>
 </div>
 
 </template>
 
 <script>
+import appTaskList from './todoComponents/appTaskList'
 export default {
   name: 'todo',
   data () {
     return {
+      data: []
     }
   },
   methods: {
   },
   components: {
+    appTaskList
   }
 }
 </script>
