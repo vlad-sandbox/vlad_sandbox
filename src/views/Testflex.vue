b8b4d27fd7febdd15b4b78146903f6d659fac4b1
Lion <Lion@ismarty.ru>
2019-06-11 19:30:05 +0300

    Изменения с flex

--- src/views/Testflex.vue
+++ src/views/Testflex.vue
@@ -2,7 +2,15 @@
   <div class="testwrapflex">
     <div class="bodyflex">
       <div class="clearflex">clear</div>
-      <div class="centerflex">center</div>
+      <div class="centerflex">
+        <div class="wrapmondey"><p>mondey</p></div>
+        <div class="wraptuesday"><p>tuesday</p></div>
+        <div class="wrapwednesday"><p>wednesday</p></div>
+        <div class="wrapthursday"><p>thursday</p></div>
+        <div class="wrapfriday"><p>friday</p></div>
+        <div class="wrapsaturday"><p>saturday</p></div>
+        <div class="wrapsunday"><p>sunday</p></div>
+      </div>
       <div class="clearflex">clear</div>
     </div>
     <div class="footerflex">footer</div>
@@ -14,10 +22,44 @@
   .bodyflex
     display flex
     height 100px
+    flex-wrap wrap
+    justify-content space-evenly
   .clearflex
     background-color red
+    flex-grow 1
   .centerflex
+    display flex
     background-color blue
+    flex-grow 1.5
+    flex-wrap wrap
+    justify-content space-evenly
   .footerflex
     background-color green
+    flex-grow 1
+  .wrapmondey
+    background-color yellow
+    flex-grow 1
+  .wraptuesday
+    background-color pink
+    flex-grow 1
+  .wrapwednesday
+    background-color brown
+    flex-grow 1
+  .wrapthursday
+    background-color black
+    flex-grow 1
+  .wrapfriday
+    background-color white
+    flex-grow 1
+  .wrapsaturday
+    background-color orange
+    flex-grow 1
+  .wrapsunday
+    background-color grey
+    flex-grow 1
+  p
+    color green
+    float right
+    margin-top 0px
+    margin-right 2px
 </style>
